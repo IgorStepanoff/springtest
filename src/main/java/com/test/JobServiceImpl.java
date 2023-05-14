7fcbadae27e36b66892542113301214a1a3a5dee
Igor Stepanov <igor.n.stepanov@gmail.com>
2023-05-14 13:09:13 +0300

    version 2

--- src/main/java/com/test/JobServiceImpl.java
+++ src/main/java/com/test/JobServiceImpl.java
@@ -2,23 +2,28 @@ package com.test;
 
 import com.test.model.User;
 import com.test.repository.UserRepository;
+import com.test.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
 @Service
-public class JobClass {
-    static Logger logger = Logger.getLogger(JobClass.class.getName());
+public class JobServiceImpl implements JobService {
+    private final Logger logger = Logger.getLogger(JobServiceImpl.class.getName());
+
     @Autowired
-    private static UserRepository userRepository;
+    private UserRepository userRepository;
 
-    public JobClass() {
+    public JobServiceImpl() {
         logger.info("Init");
+    }
 
+    public void apply() {
         if (userRepository != null) {
+            logger.warning("repository NOT null");
             userRepository.save(new User("123", "456"));
-        }else{
+        } else {
             logger.warning("repository is null");
         }
     }
--- src/main/java/com/test/TestApp.java
+++ src/main/java/com/test/TestApp.java
@@ -1,11 +1,22 @@
 package com.test;
 
+import com.test.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApp {
+public class TestApp implements CommandLineRunner {
+    @Autowired
+    private JobService job;
+
     public static void main(String[] args) {
         SpringApplication.run(TestApp.class, args);
+    }
+
+    @Override
+    public void run(String[] args) {
+        job.apply();
     }
 }
